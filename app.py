62e459843b243947d3ff00f927d4189dc98511b1
RBO25 <rimmabogrets@rambler.ru>
2022-12-22 15:08:35 +0500

    add status

--- app.py
+++ app.py
@@ -4,14 +4,15 @@ from dotenv import load_dotenv
 from flask import Flask, request
 
 
-STATUS = [
+status = [
     {'new': ""},
     {'pending': 'модератор взял в работу'},
     {'accepted': 'модерация прошла успешно'},
     {'rejected': 'модерация прошла, информация не принята'}
 ]
 
-LEVEL = [
+
+level = [
     {"winter": ""},
     {"summer": "1А"},
     {"autumn": "1А"},
@@ -19,22 +20,22 @@ LEVEL = [
 ]
 
 
-CREATE_USER_TABLER = (
-    "CREATE TABLE IF NOT EXIST user (id SERIAL PRIMARY KEY, full_name CHAR(255) NOT NULL,\
-    phone INT(11) NOT NULL, CONSTRAINT email UNIQUE (user_id), FOREIGN KEY(coord_id);"
+CREATE_USER_TABLE = (
+    "CREATE TABLE IF NOT EXIST user (full_name CHAR(255) NOT NULL,\
+    phone INT(11) NOT NULL, CONSTRAINT email UNIQUE (user_id), FOREIGN KEY (id_pereval_added);"
 )
 
 
 CREATE_TABLE_PEREVAL_ADDED = (
-    "CREATE TABLE IF NOT EXIST pereval_added (id SERIAL PRIMARY KEY, beautyTitle CHAR(255) NOT NULL,\
-     title CHAR(255) NOT NULL, other_titles CHAR(255) NOT NUL, connect CHAR(255), add_time DATETIME NOT NULL,\
-     level LEVEL);"
+    "CREATE TABLE IF NOT EXIST pereval_added (id PRIMARY KEY, beautyTitle CHAR(255) NOT NULL,\
+     title CHAR(255) NOT NULL, other_titles CHAR(255) NOT NULL, connect CHAR(255), add_time DATETIME NOT NULL,\
+     level TEXT, status DEFAULT, FOREIGN KEY(coord_id));"
 )
 
 
 CREATE_TABLE_COORDS = (
     "CREATE TABLE IF NOT EXIST coords(latitude FLOAT NOT NULL, longitude FLOAT NOT NULL, height INT NOT NULL,\
-    PRIMARY KEY (coord_id);"
+    PRIMARY KEY (coord_id), FOREIGN KEY (id_pereval_added);"
 )
 
 
@@ -42,7 +43,10 @@ CREATE_TABLE_IMAGES = (
     "CREATE TABLE IF NOT EXIST pereval_images(images_id PRIMARY KEY NOT NULL, FOREIGN KEY (id_pereval_added);"
 )
 
-ADD_STATUS = ("ALTER TABLE pereval_added ADD COLUMN IF NOT EXISTS status STATUS;")
+
+ADD_STATUS = (
+    "ALTER TABLE pereval_added ADD COLUMN IF NOT EXISTS status DEFAULT new;"
+)
 
 
 load_dotenv()
@@ -53,10 +57,14 @@ url = os.getenv("DATABASE_URL")
 connection = psycopg2.connect(url)
 
 @app.post("/api/pereval_added")
-def submitData():
+def pereval_added():
     data = request.get_json()
     status = data["status"]
     with connection:
         with connection.cursor() as cursor:
-            cursor.execute(ADD_STATUS)
-    return f'Статус{status}'
+            cursor.execute(CREATE_TABLE_PEREVAL_ADDED)
+            cursor.execute(CREATE_USER_TABLE)
+            cursor.execute(CREATE_TABLE_COORDS)
+            cursor.execute(CREATE_TABLE_IMAGES)
+            cursor.execute(ADD_STATUS, (status,))
+    return {'message': f'{status}'}
